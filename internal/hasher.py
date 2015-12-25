3638081cbc825d171139ccd2d88c251b9636233a
Ilya Mozerov <imozerov@mera.ru>
2015-12-25 17:35:36 +0300

    Implmented OrderAndMatch class

---
+++ internal/hasher.py
@@ -0,0 +1,10 @@
+class BloomierHasher:
+    def __init__(self, hash_seed, m, k, q):
+        self._hash_seed = hash_seed
+        self._m = m
+        self._k = k
+        self._q = q
+
+    def get_m(self, key):
+        #TODO
+        pass
---
+++ internal/order_and_match.py
@@ -0,0 +1,70 @@
+import sys
+
+from internal.hasher import BloomierHasher
+
+
+class OrderAndMatch:
+    def __init__(self, keys, m, k, q, hash_seed_hint = sys.maxsize + 1):
+        self._keys = keys
+        self._m = m
+        self._k = k
+        self._q = q
+        self._hash_seed = hash_seed_hint
+        self._tau = []
+        self._pi = []
+        self._order_and_match
+        self._hasher
+
+    def find(self, timeout_ms):
+        has_timed_out = False
+
+        # TODO set timer to change has_timed_out value after timeout_ms
+
+        self._hasher = BloomierHasher(self._hash_seed, self._m, self._k, self._q)
+
+        for i in range(sys.maxsize):
+            if has_timed_out:
+                raise Exception(
+                        format("Could not find order and matching for key set in alloted time with specified parameters (m=%d;k=%d;q=%d)",
+                                                             self._m, self._k, self._q))
+
+            if self._find_match(self._keys):
+                self._order_and_match = OrderAndMatch(self._hash_seed, self._pi, self._tau)
+                return self._order_and_match
+
+            self._hash_seed += 1
+
+        return self._order_and_match
+
+    def is_found(self):
+        return self._order_and_match is not None
+
+    def _find_match(self, remaining_keys):
+        if not remaining_keys:
+            return True
+
+        pi_queue = []
+        tau_queue = []
+
+        tweaker = SingletonFindingTweaker(remaining_keys, self._hasher)
+
+        for key in remaining_keys:
+            iota = tweaker.tweak(key)
+            if iota:
+                pi_queue.add(key)
+                tau_queue.add(iota)
+
+        if not pi_queue:
+            return False
+
+        remaining_keys -= pi_queue
+
+        if remaining_keys:
+            if not self._find_match(remaining_keys):
+                return False
+
+        self._pi += pi_queue
+        self._tau += tau_queue
+
+        return True
+
